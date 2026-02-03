dc304c82a8a6a73cc12445ec25e1fab8fecbbe45
Taranjeet16 <simmisodi1234@gmail.com>
2026-02-03 23:57:43 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
